fa9d8ca9294fc4998e0d798898d8774664b20f03
Matthew Phillips <matt.phillips121@hotmail.com>
2017-10-02 23:20:26 +1100

    Changed db name for reading

--- readRaw.py
+++ readRaw.py
@@ -1,6 +1,6 @@
 # Python Code to query the database “data.db” to fetch the records
 import sqlite3
-conn = sqlite3.connect('example.db')
+conn = sqlite3.connect('data.db')
 c = conn.cursor()
 c.execute('SELECT * FROM person')
 print(c.fetchall())
