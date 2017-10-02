6e1cb7b9cb93fc6306d3f927befbc6f5fa802977
Matthew Phillips <matt.phillips121@hotmail.com>
2017-10-02 23:17:51 +1100

    Initial commit of raw queries

---
+++ raw.py
@@ -0,0 +1,20 @@
+# Python code to create the two tables and insert data
+import sqlite3
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE person (
+            id INTEGER PRIMARY KEY ASC, name varchar(250) NOT NULL)''')
+
+c.execute('''CREATE TABLE address (
+            id INTEGER PRIMARY KEY ASC, street_name varchar(250),
+            street_number varchar(250), post_code varchar(250) NOT NULL,
+            person_id INTEGER NOT NULL,
+            FOREIGN KEY(person_id) REFERENCES person(id))''')
+
+c.execute(''' INSERT INTO person VALUES(1, 'Mary Poppins') ''')
+c.execute(
+        '''
+        INSERT INTO address VALUES(1, 'Magic Lane', '22', '12000', 1)
+        ''')
+conn.commit()
+conn.close()
---
+++ readRaw.py
@@ -0,0 +1,9 @@
+# Python Code to query the database “data.db” to fetch the records
+import sqlite3
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+c.execute('SELECT * FROM person')
+print(c.fetchall())
+c.execute('SELECT * FROM address')
+print(c.fetchall())
+conn.close()
